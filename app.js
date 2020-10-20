44a17256aa9faf56a1999abd4018021877c8b170
Patrick Dave Intervalo <72492512+patrickdavein@users.noreply.github.com>
2020-10-20 19:52:34 +0800

    github bckp commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const characterAmountRange = document.getElementById('characterAmountRange')
 const characterAmountNumber = document.getElementById('characterAmountNumber')
 const includeUppercaseElement = document.getElementById('includeUppercase')
@@ -5,6 +6,7 @@ const includeNumbersElement = document.getElementById('includeNumbers')
 const includeSymbolsElement = document.getElementById('includeSymbols')
 const form = document.getElementById('passwordGeneratorForm')
 const passwordDisplay = document.getElementById('passwordDisplay')
+
 
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
